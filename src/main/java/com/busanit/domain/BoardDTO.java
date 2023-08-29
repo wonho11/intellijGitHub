7ac7c18958477064ed6a280fcaf1d14bdb4eaf09
wonho111 <wonho111@gmail.com>
2023-08-29 14:36:16 +0900

    first commit

---
+++ src/main/java/com/busanit/domain/BoardDTO.java
@@ -0,0 +1,23 @@
+package com.busanit.domain;
+
+import com.busanit.entity.BoardAttach;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+public class BoardDTO {
+
+    private Long bno;
+    private String title;
+    private String content;
+    private String writer;
+    private LocalDateTime regDate;
+    private LocalDateTime updateDate;
+
+    private List<BoardAttach> uploadFiles;
+
+}
---
+++ src/main/java/com/busanit/repository/BoardAttachRepository.java
@@ -0,0 +1,8 @@
+package com.busanit.repository;
+
+import com.busanit.entity.BoardAttach;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardAttachRepository extends JpaRepository<BoardAttach, Long> {
+
+}
